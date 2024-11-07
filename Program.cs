1e91d411a5383e618e45144c56b3b5c8491fc72b
Kenn_Dormos <115053816+kennDormos@users.noreply.github.com>
2024-11-07 17:24:16 -0600

    ahora trabajaremos en el Main, utilizando todos los metodos anteriormente creados

--- Program.cs
+++ Program.cs
@@ -11,22 +11,6 @@ namespace Tarea_PrgramacionInventario
             Console.WriteLine("Bienvenido al sistema de gestion de inventario");
 
             
-            Console.Write("¿Cuántos productos desea ingresar? ");
-            int cantidad = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < cantidad; i++)
-            {
-                Console.WriteLine($"\nProducto {i + 1}:");
-                Console.Write("Nombre: ");
-                string nombre = Console.ReadLine();
-
-                Console.Write("Precio: ");
-                decimal precio = decimal.Parse(Console.ReadLine());
-
-                Producto producto = new Producto(nombre, precio);
-                inventario.AgregarProductos(producto);
-            }
-
             
 
         }
